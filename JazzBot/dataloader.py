5592aef25f5aa0c10c7453607dd023fe2bf83c4d
LeonardStockel <122832300+LeonardStockel@users.noreply.github.com>
2023-02-28 22:20:24 +0100

    v2

--- JazzBot/dataloader.py
+++ JazzBot/dataloader.py
@@ -1,6 +1,7 @@
 import numpy as np
 from vocab import *
 from music21 import *
+from torch.utils.data import Dataset, DataLoader
 import os
 
 
@@ -22,12 +23,16 @@ def noteToToken(n,l):
                             TIM_TOKS[note_offset],
                             VEL_TOKS[note_velocity]])
 
-def midiToTokens(midi_file):
+def midiToTokens(folder_path,f):
     '''
+    parameter : folder_path,f = name of the midi file
     read a midi file and extract the notes, no token start or end
     '''
     tokens = []
-    # Create a stream from the MIDI file
+    midi_file = midi.MidiFile()
+    midi_file.open(folder_path +f)
+    midi_file.read()
+    midi_file.close()
     stream = midi.translate.midiFileToStream(midi_file)
     # Iterate over the notes in the stream and extract the note information
     last_time = 0
@@ -38,75 +43,43 @@ def midiToTokens(midi_file):
         if note.isChord:
             for n in note:
                 tokens.append(noteToToken(n,last_time))
+    tokens.append([EOS]*4)
 
     return tokens
 
 def tokensToPieces(t,N):
     '''
-    cut the list of tokens into pieces of length
+    cut the list of tokens into pieces of length N, add PAD if necessary
     '''
     pieces = []
     for i in range(len(t)//(N+1)-1):
-        pieces.append(t[i:i+taille_bloc])
+        pieces.append(t[i:i+N])
+        if i == len(t)//(N+1)-2:
+            n = t[i:].length()
+            list_pad = [PAD]*4
+            pieces.append(t[i:]+(N-n)*[list_pad])
     return pieces
 
 def pieceToInputTarget(p):
     '''
     return a couple of vectors (input, target) with input = SOS + piece; target = piece + EOS
     '''
-    target = vectorize()
+    input_p = [[SOS]*4]+[custom_vocab(tok) for tok in p]
+    target_p = [custom_vocab(tok) for tok in p]+[[EOS]*4]
+    return(input_p,target_p)
 
-
-
-folder_path = "../Data" 
-    
-# Load the MIDI file
-midi_file = midi.MidiFile()
-
-les_tokens = []
-
-# Get all the file names in the folder
-file_names = os.listdir(folder_path)
-for f in file_names:
-    print(f)
-    midi_file = midi.MidiFile()
-    midi_file.open(folder_path +f)
-    midi_file.read()
-    midi_file.close()
-    # Create a stream from the MIDI file
-    stream = midi.translate.midiFileToStream(midi_file)
-
-    # Iterate over the notes in the stream and extract the note information
-    last_time = 0
-
-    for note in stream.flat.notes:
-        if note.isNote:
-            les_tokens.append(noteToToken(note))
-
-        if note.isChord:
-            for n in note:
-                les_tokens.append(noteToToken(n))
-
-
-#répartir le data du morceau en blocs de 120 attributs (30 notes)
-#Et associer à chaque bloc la réponse attendue (l'attribut suivant)
-
-taille_bloc = 120
-les_morceaux = []
-les_morceaux_shift = []
-
-for i in range(len(les_tokens)//(taille_bloc+1)-1):
-    les_morceaux.append(les_tokens[i:i+taille_bloc])
-    les_morceaux_shift.append(les_tokens[i+1:i+1+taille_bloc])
-
-
-def vectorize(i):
-    return [0]*i + [1] + [0]* (len(custom_vocab)-i-1)
-def unvectorize(v):
-    for i in range(len(v)):
-        if v[i]:
-            return i
-
-input_vect = [ [vectorize(custom_vocab[tok]) for tok in morceau] for morceau in les_morceaux ]
-rep_vect = [ [vectorize(custom_vocab[tok]) for tok in morceau] for morceau in les_morceaux_shift ]
-        
+def midisToVectInputTarget(folder_path,N):
+    '''
+    parameters : folder_path, N = size of pieces
+    '''
+    vectInput = []
+    vectTarget = []
+    file_names = os.listdir(folder_path)
+    for f in file_names:
+        tokens = midiToTokens(folder_path,f)
+        pieces = tokensToPieces(t,N)
+        for p in pieces:
+            input,target = pieceToInputTarget(p)
+            vectInput.append(input)
+            vectTarget.append(target)
+    return vectInput,vectTarget
--- JazzBot/vocab.py
+++ JazzBot/vocab.py
@@ -4,7 +4,9 @@ from collections import OrderedDict
 #special tokens
 SOS = "sos" #start
 EOS = "eos" #end
-#PAD = "xx"  #inutile si la data est de longueur définie
+PAD = "xxx"  #NAN
+
+SPECIAL = [SOS,EOS,PAD]
 
 NOTE_SIZE = 128
 DUR_SIZE = 160
@@ -16,20 +18,31 @@ DUR_TOKS = [f'd{i}' for i in range(DUR_SIZE)]
 TIM_TOKS = [f't{i}' for i in range(TIM_SIZE)]
 VEL_TOKS = [f'v{i}' for i in range(VEL_SIZE)]
 
-VOCAB_NOTE = [SOS,EOS] + NOTE_TOKS 
-VOCAB_DUR = [SOS,EOS] + DUR_TOKS 
-VOCAB_TIM = [SOS,EOS] + TIM_TOKS
-VOCAB_VEL = [SOS,EOS] + VEL_TOKS
+VOCAB_NOTE =  NOTE_TOKS + SPECIAL
+VOCAB_DUR = DUR_TOKS + SPECIAL
+VOCAB_TIM = TIM_TOKS + SPECIAL
+VOCAB_VEL = VEL_TOKS + SPECIAL
 
-##créer 4 vocabs, possible ?
-DICT = [(element, index) for index, element in enumerate(VOCAB)]
+DICT_NOTE = [(element, index) for index, element in enumerate(VOCAB_NOTE)]
+DICT_DUR = [(element, index) for index, element in enumerate(VOCAB_DUR)]
+DICT_TIM = [(element, index) for index, element in enumerate(VOCAB_TIM)]
+DICT_VEL = [(element, index) for index, element in enumerate(VOCAB_VEL)]
 
-custom_vocab = vocab(OrderedDict(DICT))
+CV_NOTE = vocab(OrderedDict(DICT_NOTE))
+CV_DUR = vocab(OrderedDict(DICT_DUR))
+CV_TIM = vocab(OrderedDict(DICT_TIM))
+CV_VEL = vocab(OrderedDict(DICT_VEL))
 
-def itos(voc, i):
-    return voc.get_itos()[i]
+itos_NOTE = CV_NOTE.get_itos()
+itos_DUR = CV_DUR.get_itos()
+itos_TIM = CV_TIM.get_itos()
+itos_VEL = CV_VEL.get_itos()
 
-def stoi(voc, s):
-    return voc[s]
-
-itos_vocab = custom_vocab.get_itos()
+def custom_vocab(tok):
+    '''
+    tok = [note,dur,tim,vel]
+    '''
+    return [CV_NOTE[tok[0]],
+            CV_DUR[tok[1]],
+            CV_TIM[tok[2]],
+            CV_VEL[tok[3]]]
