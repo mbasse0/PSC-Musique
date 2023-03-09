b82fbf8d26fa68c1c6d831675a01ac8b6d6685c2
LeonardStockel <122832300+LeonardStockel@users.noreply.github.com>
2023-03-09 13:16:34 +0100

    v3

--- JazzBot/test.py
+++ JazzBot/test.py
@@ -2,15 +2,20 @@ from config import *
 from model import *
 from data_processor import *
 
-# model = torch.load("../model.pth")
-# model.eval()
+model = torch.load("../model.pth")
+model.eval()
 
 
 toks = midiToTokens(path, "A Thousand Miles - Vanessa Carlton - Verse-And-Pre-Chorus.mid")
 y_input = torch.tensor(pieceToInputTarget(tokensToPieces(toks,N)[0])[0]).to(device)
-print(pieceToInputTarget(tokensToPieces(toks,N)[0]))
-# tgt_mask = model.get_tgt_mask(y_input.size(0)).to(device)
-# pred = model(torch.tensor([0]*(N+3)).to(device), y_input, tgt_mask)
-# next_item = pred.topk(1)[1].view(-1).item()
-# print([pred.topk(1)[1].view(-1)[i].item() for i in range(100)])
-# print(itos[next_item])
+print(len(y_input))
+tgt_mask = model.get_tgt_mask(y_input.size(0)).to(device)
+pred = model(torch.tensor([0]*(N-1)).to(device), y_input, tgt_mask)
+# Get the index of the highest probability for each token in the sequence
+predicted_tokens = torch.argmax(pred, dim=2)
+
+# Convert the indices to token strings using the CV vocabulary
+predicted_strings = [itos[token.item()] for token in predicted_tokens[0]]
+
+# Print the predicted token sequence as a string
+print(" ".join(predicted_strings))
