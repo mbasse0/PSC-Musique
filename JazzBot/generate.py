29165628c6db3f0bee54878b10730d0b8dcc2cc0
Maxime Basse <maxime.basse@gmail.com>
2023-03-21 19:22:31 +0100

    tqdm sur generate

--- JazzBot/generate.py
+++ JazzBot/generate.py
@@ -1,6 +1,5 @@
 from config import *
 from model import *
-from data_processor import *
 
 import torch.nn as nn
 import torch.nn.functional as F
@@ -11,20 +10,9 @@ import numpy as np
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-# # On charge le modèle pré entraîné
-# model = Transformer(
-# num_tokens=len(custom_vocab), dim_model=512, num_heads=8, num_encoder_layers=1, num_decoder_layers=4, dropout_p=0.1)
-# model.load_state_dict(torch.load("model4out.pth"))
-# # On déplace le modèle sur le GPU si c'est possible
-# model.to(device)
-# # On met le modèle en mode inférence pour pas avoir le dropout et obtenir des prédictions plus précises
-# model.eval()
-
-
-
-
 
 def generate_sequence(model, start_tokens, max_length=100, temperature=1.0):
+    # start_tokens doit être une liste d'indices
     model.eval()
 
     # L'input donné à l'encoder (vecteur nul dans notre cas, comme pendant l'entraînement)
@@ -32,18 +20,18 @@ def generate_sequence(model, start_tokens, max_length=100, temperature=1.0):
     X = torch.tensor([0]*taille_bloc).unsqueeze(0).to(device)
 
     with torch.no_grad():
-        tokens = start_tokens
-        for _ in range(max_length - len(start_tokens)):
+        les_tokens = start_tokens
+        for _ in tqdm(range(max_length - len(start_tokens))):
             # Unsqueeze(0) rajoute une dimension qui correspond au batch_size (qui vaut 1 dans ce cas) pour coller aux shape attendues par le modèle
-            input_tokens = torch.tensor(tokens).unsqueeze(0).to(device)
+            input_tokens = torch.tensor(start_tokens).unsqueeze(0).to(device)
             
             output = model(X, input_tokens)
             # Les logits sont des probabilits non normalisées. La température contrôle leur dispersion : permet d'ajouter plus ou moins de bruit lors de la prédiction
             logits = output[:, -1, :] / temperature
             # Softmax transforme les logits en probabilités, multinomial fait une séleciton pondérée par ces probabilités d''un seul indice (num_samples=1), to_list passe de tensor à array
             next_token = torch.multinomial(F.softmax(logits, dim=-1), num_samples=1).squeeze().tolist()
-            tokens.append(next_token)
+            les_tokens.append(next_token)
 
-    return tokens
+    return les_tokens
 
 
