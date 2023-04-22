7ec8d68fe9c9bf627acdc06769232a748dee9b65
pierreaguie <pierre.aguie@gmail.com>
2023-04-23 00:01:17 +0200

    GROSSES MODIF MAIN

--- JazzBot_Lightning/main.py
+++ JazzBot_Lightning/main.py
@@ -1,89 +1,91 @@
 from vocab import *
 from model import *
 from model4out import *
-import numpy as np
 from generate import *
 from data_decoder import *
-##from train import *
+from data_encoder import *
 from dataset import *
-import pytorch_lightning as pl
-from config import *
-import sys
 from csv_encoder import *
+from config import *
+
+import pytorch_lightning as pl
+import numpy as np
+import sys
 
 def main(argv):
    """
-   arg1 : 0 : model , 1 : model4out
-   arg2 : 0 : noDDP , 1 : DDP
-   arg3 : nb_epochs
-   arg4 : learning_rate (Optionnel)
-   arg5 : batch_size (Optionnel)
+   arg0 : "train" : entraîner , "gen" : génére
+   arg1 : model_name
+   arg2 : 0 : model , 1 : model4out
+
+   (arguments uniquement valables pour "train")
+   arg3 : 0 : noDDP , 1 : DDP
+   arg4 : nb_epochs
+   arg5 : dataset_name
+
+   (arguments uniquement valables pour "gen")
+   arg3 : nb_generated_tokens
+   arg4 : start_midi_name (optionnel)
+   arg5 : temperature (optionnel)
    """
-   if len(argv) == 3:
-      learning_rate = 0.05
+
+   if argv[0] == "train":
+      ## ENTRAINEMENT
       batch_size = 32
-   else:
-      learning_rate = float(argv[3])
-      batch_size = float(argv[4])
 
-   nb_epochs = float(argv[2])
+      nb_epochs = int(argv[4])
 
-   # input_vect = np.load('input_weimar.npy')
-   # rep_vect = np.load('rep_weimar.npy')
+      dataset_path = "./Datasets/" + argv[5]
+      input_vect, rep_vect = tokensFileToVectInputTarget(dataset_path,120)
+      train_dataloader, val_dataloader = get_two_dataloaders(input_vect, rep_vect, batch_size)
+
+      if int(argv[2]) == 1:
+         model = Transformer4(
+            dim_model=512, num_heads=8, num_encoder_layers=1, num_decoder_layers=6, dropout_p=0.1, learning_rate = 0.05
+         )
+      else:
+         model = Transformer(
+            num_tokens=len(custom_vocab), dim_model=512, num_heads=8, num_encoder_layers=1, num_decoder_layers=4, dropout_p=0.1, learning_rate= 0.05
+         )
+
+      logger = pl.loggers.TensorBoardLogger(save_dir='.')
+
+      if int(argv[1]) == 1:
+         trainer = pl.Trainer(accelerator='gpu', gpus=3, strategy='ddp', max_epochs=nb_epochs, log_every_n_steps=20, benchmark=True, profiler="simple", logger=logger)
+      else:
+         trainer = pl.Trainer(accelerator='gpu', gpus=1, max_epochs=nb_epochs, log_every_n_steps=20, benchmark=True, profiler="simple", logger=logger)
+
+      trainer.fit(model, train_dataloader, val_dataloader)
+
+      model_path = "./Models/" + argv[1]
+      torch.save(model.state_dict(), model_path)
    
-   # # Weimar120
-   input_vect, rep_vect = tokensFileToVectInputTarget("./Datasets/SmallWeimar.csv",120)
+   elif argv[0] == "gen":
+      ## GENERATION
+      if len(argv) == 4:
+         start_tokens = [custom_vocab["n60"], custom_vocab["d1"], custom_vocab["t1"], custom_vocab["v64"]]
+         temp = 1.0
+      else:
+         midi_path = "./Midis/" + argv[4]
+         start_tokens = midiToTokens(midi_path)
+         temp = float(argv[5])
 
-   #np.save('input_weimar120.npy', input_vect)
-   #np.save('rep_weimar120.npy', rep_vect)
+      if int(argv[2]) == 1:
+         model = Transformer4(
+            dim_model=512, num_heads=8, num_encoder_layers=1, num_decoder_layers=6, dropout_p=0.1, learning_rate = 0.05
+         )
+      else:
+         model = Transformer(
+            num_tokens=len(custom_vocab), dim_model=512, num_heads=8, num_encoder_layers=1, num_decoder_layers=4, dropout_p=0.1, learning_rate=0.05
+         )
 
-   # input_vect = np.load('input_weimar120.npy')
-   # rep_vect = np.load('rep_weimar120.npy')
+      model_path = "./Models/" + argv[1]
+      model.load_state_dict(torch.load(model_path))
 
-   # ## CREATION DATASET ET DATALOADER
-
-   # dataloader = get_dataloader(input_vect, rep_vect, batch_size)
-   train_dataloader, val_dataloader = get_two_dataloaders(input_vect, rep_vect, batch_size)
-
-
-   ## ENTRAINEMENT
-
-   if int(argv[0]) == 1:
-      model = Transformer4(
-         dim_model=512, num_heads=8, num_encoder_layers=1, num_decoder_layers=6, dropout_p=0.1, learning_rate = learning_rate
-      )
-   else:
-      model = Transformer(
-         num_tokens=len(custom_vocab), dim_model=512, num_heads=8, num_encoder_layers=1, num_decoder_layers=4, dropout_p=0.1, learning_rate=learning_rate
-      )
-
-   logger = pl.loggers.TensorBoardLogger(save_dir='.')
-
-   if int(argv[1]) == 1:
-      trainer = pl.Trainer(accelerator='gpu', gpus=3, strategy='ddp', max_epochs=nb_epochs, log_every_n_steps=20, benchmark=True, profiler="simple", logger=logger)
-   else:
-      trainer = pl.Trainer(accelerator='gpu', gpus=1, max_epochs=nb_epochs, log_every_n_steps=20, benchmark=True, profiler="simple", logger=logger)
-
-   trainer.fit(model, train_dataloader, val_dataloader)
-
-   # save the model weights to a file
-   torch.save(model.state_dict(), './Models/model_5_deter_defaultloss_512_8_1_4_0.1_0.05.pth')
-
-
-   # ##GENERER SEQ
-
-   # # Define the start tokens for your inference. start_tokens expects an array of indices in the vocab
-   # start_tokens = [custom_vocab["n60"], custom_vocab["d1"], custom_vocab["t1"], custom_vocab["v64"]]
-   # # Generate a sequence of tokens
-   # model.load_state_dict(torch.load('model4out_rect.pth'))
-   # generated_tokens = generate_sequence(model, start_tokens, max_length=300, temperature=1)
-
-   # # Decode the generated tokens into the original format
-   # decoded_tokens = [itos_vocab[el] for el in generated_tokens]
-   # #On peut sauver la prédiction dans un array
-   # #np.save("generation4.npy", decoded_tokens)
-   # print("Generated sequence:", decoded_tokens)
-
+      nb_tokens = int(argv[3])
+      generated_tokens = generate_sequence(model, start_tokens, max_length=nb_tokens, temperature = temp)
+      decoded_tokens = [itos_vocab[el] for el in generated_tokens]
+      print(decoded_tokens)
 
    # ## CONVERSION DE LA SEQUENCE EN MIDI
 
