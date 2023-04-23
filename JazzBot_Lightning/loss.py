f1fc8cb18f662da723c5b506180a47c96fb27cd7
pierreaguie <pierre.aguie@gmail.com>
2023-04-23 12:30:43 +0200

    fix

--- JazzBot_Lightning/loss.py
+++ JazzBot_Lightning/loss.py
@@ -35,7 +35,7 @@ class tokenTypeLoss(nn.Module):
         mask = torch.Tensor((32, 120)).to(device)
         for i in range(batch_size):
             for j in range(len(max_indices[0])):
-                mask[i][j] = itos_vocab[max_indices[i][j]][0] != itos_vocab[target[i][j]][0]
+                mask[i,j] = float(itos_vocab[max_indices[i,j]][0] != itos_vocab[target[i,j]][0])
         high_cost = self.weight * (loss * mask.float()).mean()
         # return loss, pct_err
         return loss + high_cost
