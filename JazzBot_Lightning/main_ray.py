834c2d418bf279fa4a7ae61576f565fbe6d0c560
hduprieu <116175147+hduprieu@users.noreply.github.com>
2023-04-25 02:15:34 +0200

    zert

--- JazzBot_Lightning/main_ray.py
+++ JazzBot_Lightning/main_ray.py
@@ -71,7 +71,7 @@ def main(argv):
     resources_per_trial = {"cpu": 1, "gpu": gpus_per_trial}
     
     def stopper(trial_id, result):
-        return math.isnan(result["loss"]) or ("training_iteration" in result) or ("training_iter" in result)
+        return math.isnan(result["loss"]) or ("training_iteration" in result)
 
     tuner = tune.Tuner(
         tune.with_resources(
