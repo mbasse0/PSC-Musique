98d0dd4cb4bcb03b7794392ec2144616f5b92f63
pierreaguie <pierre.aguie@gmail.com>
2023-04-29 00:43:00 +0200

    comparator

---
+++ GPU comparator/comparator.py
@@ -0,0 +1,13 @@
+import torch
+import time
+
+t_0 = time.time()
+device = "cuda" if torch.cuda.is_available() else "cpu"
+N = 100000
+
+sum = torch.ones(size = (100,100), device=device)
+
+for _ in range(N):
+    sum += torch.ones(size = (100,100), device=device).to(device)
+
+print(time.time() -t_0)
