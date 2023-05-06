4e18c55fc25d3c4b8d326190c4a873deb962b15a
hduprieu <116175147+hduprieu@users.noreply.github.com>
2023-05-06 19:06:00 +0200

    TIME NOT DURATION

--- JazzBot_Lightning/model.py
+++ JazzBot_Lightning/model.py
@@ -122,7 +122,7 @@ class Transformer(pl.LightningModule):
         pred = pred.permute(0, 2, 1)
         # lossF = nn.CrossEntropyLoss()
         # lossF = tokenTypeLoss(3.)
-        lossF = rhythmLoss([4.,1.,2.,1.,0.5])
+        lossF = rhythmLoss([4.,1.,1.,3.,0.5])
         # lossF = harmonicLoss([3.,1.], [0.6, 0.6, 0.2, 0.2, 0.2, 0.2, 1.5])
         loss = lossF(pred, y_expected)
         self.log("ptl/train_loss", loss)
