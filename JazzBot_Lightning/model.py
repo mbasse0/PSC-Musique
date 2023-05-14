9e2600d4736cfd7d3a1d29a22bfa3fc5d98b21ca
hduprieu <116175147+hduprieu@users.noreply.github.com>
2023-05-15 00:33:55 +0200

    no scheduler

--- JazzBot_Lightning/model.py
+++ JazzBot_Lightning/model.py
@@ -96,8 +96,9 @@ class Transformer(pl.LightningModule):
 
     
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=self.lr, momentum=0.8)
-        return optimizer
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.lr)
+        #scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.2, last_epoch=- 1, verbose=False)
+        return {"optimizer": optimizer, "lr_scheduler": scheduler}
     
     def training_step(self, batch):
         y_input, y_expected = batch
