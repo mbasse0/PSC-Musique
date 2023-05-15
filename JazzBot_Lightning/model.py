55722b874b61e64c2682ffd685278a2a7835414d
hduprieu <116175147+hduprieu@users.noreply.github.com>
2023-05-15 14:57:10 +0200

    no lr schedule

--- JazzBot_Lightning/model.py
+++ JazzBot_Lightning/model.py
@@ -97,8 +97,9 @@ class Transformer(pl.LightningModule):
     
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(self.parameters(), lr=self.lr)
-        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.5, last_epoch=- 1, verbose=False)
-        return {"optimizer": optimizer, "lr_scheduler": scheduler}
+        #scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.5, last_epoch=- 1, verbose=False)
+        #return {"optimizer": optimizer, "lr_scheduler": scheduler}
+        return optimizer
     
     def training_step(self, batch):
         y_input, y_expected = batch
